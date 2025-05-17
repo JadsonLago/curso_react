88a75f0367784b7ca2e2d7f6c84725f3b5a14b71
JadsonLago <jadsonlago.consulltoria@gmail.com>
2025-05-17 00:03:55 -0300

    feat: criacao de class component

---
+++ 1_componentes/src/components/ClassComponent.jsx
@@ -0,0 +1,17 @@
+//importar o react
+import React from "react";
+
+
+class ClassComponent extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <h2>Componente de classe</h2>
+            </div>
+        )
+
+    }
+}
+
+export default ClassComponent;
---
+++ 1_componentes/src/components/FunctionalCompenent.jsx
@@ -0,0 +1,12 @@
+//Componente
+
+function FunctionalCompenent(params) {
+    //Retorna um objeto JSX
+    return(
+        <h2>Testando Componente</h2>
+    );
+    
+}
+
+//Exporta componente
+export default FunctionalCompenent;
